4ead4ba64e26d30ab22ffbc428524ebb26c11d54
Krzysztof Wencel <kwencel@users.noreply.github.com>
2019-06-12 21:58:27 +0200

    Trying to fix permission errors #4

--- pipeline-iam.tf
+++ pipeline-iam.tf
@@ -51,7 +51,8 @@ resource "aws_iam_policy" "codebuild" {
         "s3:*",
         "cloudformation:DescribeStacks",
         "cloudformation:CreateStack",
-        "cloudformation:DescribeStackEvents"
+        "cloudformation:DescribeStackEvents",
+        "cloudformation:DescribeStackResource"
       ]
     },
     {
